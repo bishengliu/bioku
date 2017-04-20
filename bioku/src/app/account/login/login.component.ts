46b047b15e0524fad50dea6fb46b586a65268360
b_liu <b.liu@nki.nl>
2017-04-20 22:37:49 +0200

    add navigation after login

--- bioku/src/app/account/login/login.component.ts
+++ bioku/src/app/account/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import {FormBuilder, AbstractControl, FormGroup, Validators} from '@angular/forms';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import {Store} from 'redux';
+import { Router} from '@angular/router';
 import{AlertService} from '../../_services/AlertService';
 import { AppSetting} from '../../_config/AppSetting';
 import {APP_CONFIG} from '../../_providers/AppSettingProvider';
@@ -25,7 +26,7 @@ export class LoginComponent implements OnInit {
   isLogin: boolean = false;
   user: User = null;
 
-  constructor(fb: FormBuilder, private alertService: AlertService, private loginService: LoginService, @Inject(APP_CONFIG) private appSetting: any, @Inject(AuthStore) private authStore: Store<AuthState>) { 
+  constructor(fb: FormBuilder, private alertService: AlertService, private loginService: LoginService, @Inject(APP_CONFIG) private appSetting: any, @Inject(AuthStore) private authStore: Store<AuthState>, private router: Router) { 
     this.loginForm = fb.group({
       'username': ['', Validators.required],
       'password': ['', Validators.required]
@@ -49,6 +50,9 @@ export class LoginComponent implements OnInit {
     let state= this.authStore.getState()
     this.user = state.authUser;
     this.isLogin = state.token? true: false;
+    if(this.isLogin){
+      this.router.navigate(['home']);
+    }
   }
   ngOnInit() {
   }
