49bf7c7c65a83e63021b21edcd6e3cbe68df2d7d
b_liu <b.liu@nki.nl>
2017-04-20 19:12:57 +0200

    try to fix this issue failed: ERROR in Error encountered resolving symbol values statically

--- bioku/src/app/_providers/ReduxProviders.ts
+++ bioku/src/app/_providers/ReduxProviders.ts
@@ -5,7 +5,7 @@ export const AuthStore = new InjectionToken<any>("login.store");
 //auth store instance to be injected
 import {authStore} from '../_redux/login/login_store';
 //interface is not used for di, which is not available at runtime
-//use InjectionToken
+// use InjectionToken
 export const StoreProviders: Array<any> = [
     //login
     { provide: AuthStore, useValue: authStore }
--- bioku/src/app/_redux/login/login_store.ts
+++ bioku/src/app/_redux/login/login_store.ts
@@ -4,6 +4,6 @@ import { AuthState } from './login_state';
 import { authReducer } from './login_reducers';
 
 //using redux devtools for chrome
-let  devtools: StoreEnhancer<AuthState> = window['devToolsExtension'] ? window['devToolsExtension']() : f => f;
+const devtools: StoreEnhancer<AuthState> = window['devToolsExtension'] ? window['devToolsExtension']() : f => f;
 
-export let authStore: Store<AuthState> = createStore<any>(authReducer, devtools, applyMiddleware(thunk));
+export const authStore: Store<AuthState> = createStore<any>(authReducer, devtools, applyMiddleware(thunk));
