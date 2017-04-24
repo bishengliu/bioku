eba4ca68f2213c2154a29258e07ca6221989fae3
b_liu <b.liu@nki.nl>
2017-04-24 23:34:01 +0200

    working on file upload

--- bioku/src/app/_providers/APIServiceProviders.ts
+++ bioku/src/app/_providers/APIServiceProviders.ts
@@ -1,10 +1,12 @@
 import { LoginService } from '../_services/LoginService';
 import { LogoutService } from '../_services/LogoutService';
 import { RegisterService } from '../_services/RegisterService';
+import { UploadService } from '../_services/UploadService';
 
 
 export var APIServiceProviders: Array<any> = [
         {provide: LoginService, useClass: LoginService},
         {provide: LogoutService, useClass: LogoutService},
         {provide: RegisterService, useClass: RegisterService},
+        {provide: UploadService, useClass: UploadService},
     ];
