0faebac0c40cc43517fa80ea2f47437b04b69c72
b_liu <b.liu@nki.nl>
2017-05-23 23:34:32 +0200

    working on adding containers to groups

--- bioku/src/app/admin/containers/container-list/container-list.component.ts
+++ bioku/src/app/admin/containers/container-list/container-list.component.ts
@@ -11,6 +11,7 @@ import { Observable } from 'rxjs';
 
 import { AlertService } from '../../../_services/AlertService';
 import { ContainerService} from '../../../_services/ContainerService';
+import {GroupService} from '../../../_services/GroupService';
 @Component({
   selector: 'app-container-list',
   templateUrl: './container-list.component.html',
@@ -18,10 +19,12 @@ import { ContainerService} from '../../../_services/ContainerService';
 })
 export class ContainerListComponent implements OnInit {
   containers: Observable<Array<Container>>;
+  groups: Observable<Array<Group>>;
   appUrl: string;
   tableView: boolean = false;
 
-  constructor(@Inject(APP_CONFIG) private appSetting: any, @Inject(AppStore) private appStore, private router: Router, private alertService: AlertService, private containerService: ContainerService)
+  constructor(@Inject(APP_CONFIG) private appSetting: any, @Inject(AppStore) private appStore, private router: Router, 
+              private alertService: AlertService, private containerService: ContainerService, private groupService: GroupService)
   { 
     this.appUrl = this.appSetting.URL;
   }
@@ -55,8 +58,27 @@ export class ContainerListComponent implements OnInit {
     }
     return array;
   }
+
+  add2Group(container_pk: number, ctl: any): void{
+    console.log('container: ' + container_pk);
+    console.log('group: ' + ctl.value);
+  }
+  removefromGroup(container_pk: number, group_pk: number){
+    console.log('container: ' + container_pk);
+    console.log('group: ' + group_pk);
+  }
+  isGroup(groups: Array<Group>, group: Group){
+    let found: Boolean = false;
+    if (groups == null){
+      return found;
+    }
+    let pks: Array<number> = groups.map(g=>g.pk);   
+    pks.indexOf(group.pk) !=-1 ? found=true : found=false;
+    return found;
+  }
   ngOnInit() {
     this.containers = this.containerService.getAllContainers();
+    this.groups = this.groupService.getAllGroups();
   }
 
 }
