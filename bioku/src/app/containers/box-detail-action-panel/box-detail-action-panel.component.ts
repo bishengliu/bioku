0ddabf8f5133df872163ae58ed588c7b6be083bf
b_liu <b.liu@nki.nl>
2017-06-04 00:49:47 +0200

    working on side action panel

--- bioku/src/app/containers/box-detail-action-panel/box-detail-action-panel.component.ts
+++ bioku/src/app/containers/box-detail-action-panel/box-detail-action-panel.component.ts
@@ -9,6 +9,8 @@ import { ContainerService } from '../../_services/ContainerService';
 //redux
 import { AppStore } from '../../_providers/ReduxProviders';
 import { AppState } from '../../_redux/root/state';
+//mydatepicker
+import {IMyOptions} from 'mydatepicker';
 
 @Component({
   selector: 'app-box-detail-action-panel',
@@ -23,6 +25,18 @@ export class BoxDetailActionPanelComponent implements OnInit {
   box: Box = null;
   boxDescription: string;
   samples: Array<Sample> =[];
+  freezing_date = {}; //for only select one sample
+  action_panel_msg: string= null;
+  //mydatepicker
+  private myDatePickerOptions: IMyOptions = {
+        // other options...
+        todayBtnTxt: 'Today',
+        dateFormat: 'yyyy-mm-dd',
+        openSelectorTopOfInput: true,
+        showSelectorArrow: false,
+        editableDateField: false,
+        openSelectorOnInputClick: true};
+  
   constructor(@Inject(APP_CONFIG) private appSetting: any, @Inject(AppStore) private appStore, private containerService: ContainerService,) { 
     this.appUrl = this.appSetting.URL;
     //subscribe store state changes
@@ -50,6 +64,36 @@ export class BoxDetailActionPanelComponent implements OnInit {
     }
     return samples;
   }
-  ngOnInit() {}
 
+  parseFreezingDate(date: string){
+    console.log(date);
+      let freezing_date = {};
+      if(date){
+        let dArray = date.split('-');
+        freezing_date ={date: {year: +dArray[0], month: +dArray[1], day: +dArray[2]}};
+      }
+      console.log(freezing_date);
+      return freezing_date;
+  }
+
+  updateSampleDetail(value:any, box_position: string, data_attr: string, required: boolean){
+    console.log(value);
+    if((value == "" || value == null) && required){
+      this.action_panel_msg = data_attr + " is required!"
+    }
+    else
+    {
+      this.action_panel_msg = null;
+      //this.containerService.updateBoxDescription(this.container.pk, box_position, data_attr, value:any)
+      //   .subscribe(()=>{},(err)=>console.log(err));
+    }
+    
+  }
+  ngOnInit() {}
+  ngOnChanges(){
+    if (this.selectedSamples != null && this.selectedSamples.length==1){
+      let samples = this.findSamples(this.selectedSamples);
+      this.freezing_date = this.parseFreezingDate(samples[0].freezing_date); //clear selected samples
+    }
+  }
 }
