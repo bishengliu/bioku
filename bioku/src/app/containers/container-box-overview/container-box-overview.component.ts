9372a565f9454617ba1b6908155132ad6e329f7e
Bisheng Liu <b.liu@nki.nl>
2017-08-31 11:51:05 +0200

    working on passing async data to child cmp

--- bioku/src/app/containers/container-box-overview/container-box-overview.component.ts
+++ bioku/src/app/containers/container-box-overview/container-box-overview.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Input } from '@angular/core';
+import { SimpleChanges } from '@angular/core';
+import { Container } from '../../_classes/Container';
+import { ContainerTower, Containershelf, BoxAvailability } from '../../_classes/ContainerTower';
 
 @Component({
   selector: 'app-container-box-overview',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container-box-overview.component.css']
 })
 export class ContainerBoxOverviewComponent implements OnInit {
+  @Input() container: Container;
+  @Input() towers: Array<ContainerTower>;
 
+  _towers: Array<ContainerTower>;
   constructor() { }
 
   ngOnInit() {
+    console.log('----------------------');
+    console.log(this.container);
+    console.log(this.towers);
+    console.log('----------------------');
   }
 
+  ngOnChanges(change: SimpleChanges){
+    if(change['towers']){
+      console.log(change['towers']);
+    }
+  }
 }
