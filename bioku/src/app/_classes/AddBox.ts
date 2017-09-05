2a509898ed752c523ec835ae003cd2adbb2c65c9
Bisheng Liu <b.liu@nki.nl>
2017-09-05 15:08:50 +0200

    working on add boxes

---
+++ bioku/src/app/_classes/AddBox.ts
@@ -0,0 +1,4 @@
+export class AddBox{
+    is_excluded: boolean;
+    box_full_position: string;
+}
--- bioku/src/app/containers/container-box-add/container-box-add.component.ts
+++ bioku/src/app/containers/container-box-add/container-box-add.component.ts
@@ -7,10 +7,12 @@ import { AppSetting} from '../../_config/AppSetting';
 import { APP_CONFIG } from '../../_providers/AppSettingProvider';
 import { User } from '../../_classes/User';
 import { Box } from '../../_classes/Box';
+import { AddBox } from '../../_classes/AddBox';
 import { Container } from '../../_classes/Container';
 import { ContainerTower, Containershelf, BoxAvailability } from '../../_classes/ContainerTower';
 import {  ContainerService } from '../../_services/ContainerService';
 import { LocalStorageService } from '../../_services/LocalStorageService';
+import { UtilityService } from '../../_services/UtilityService';
 //redux
 import { AppStore } from '../../_providers/ReduxProviders';
 import { AppState , AppPartialState} from '../../_redux/root/state';
@@ -30,18 +32,18 @@ export class ContainerBoxAddComponent implements OnInit, OnDestroy {
   //CURRENT CONTAINER
   container: Container;
   boxes: Array<BoxAvailability> = new Array<BoxAvailability>();
-  //container towers
-  //container shevels
-  //container box
-  container_tower: number;
-  container_shelf: number;
-  container_box: number;
+  add_boxes: Array<AddBox> = new Array<AddBox>();
+  box_horizontal: number;
+  box_vertical: number;
   
-  constructor(private route: ActivatedRoute, @Inject(APP_CONFIG) private appSetting: any, @Inject(AppStore) private appStore, 
+  constructor(private route: ActivatedRoute, @Inject(APP_CONFIG) private appSetting: any, @Inject(AppStore) private appStore, private utilityService: UtilityService,
               private router: Router, private http: Http, private containerService: ContainerService, private localStorageService: LocalStorageService)
   { 
     appStore.subscribe(()=> this.updateState());
     this.updateState();
+    //SET THE DEFAULT BOX LAYOUT
+    this.box_horizontal = this.appSetting.box_horizontal;
+    this.box_vertical = this.appSetting.box_vertical;
   }
   updateState(){
     let state= this.appStore.getState();
@@ -65,13 +67,22 @@ export class ContainerBoxAddComponent implements OnInit, OnDestroy {
       else{
         return this.containerService.containerDetail(this.id);}
     });
-
     //container and boxes observable
     this.sub.subscribe(
       (container: Container)=> { this.container = container; }, 
       (err)=>{console.log(err)}
     );
-    this.boxes = this.localStorageService.selectedEmptySlots;
+    this.boxes = this.localStorageService.selectedEmptySlots.sort(this.utilityService.sortArrayBySingleProperty('full_position'));;
+    //generate add_boxes
+    this.boxes.forEach((ab, i)=>{
+      let add_box: AddBox = new AddBox();
+      add_box.box_full_position = ab.full_position;
+      add_box.is_excluded = false;
+      this.add_boxes.push(add_box);
+    });
+    //get the first box
+    this.box_horizontal = (this.container.boxes != null && this.container.boxes.length > 0) ? this.container.boxes[0].box_horizontal : this.box_horizontal;
+    this.box_vertical = (this.container.boxes != null && this.container.boxes.length > 0) ? this.container.boxes[0].box_vertical : this.box_vertical;
   }
   ngOnDestroy() { this.sub.unsubscribe(); }
 }
