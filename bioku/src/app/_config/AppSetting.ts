255e6ca887ca387869e07e6b962e7a9a90591a51
b_liu <b.liu@nki.nl>
2017-10-14 21:35:41 +0200

    FIX SOME ISSUES IN THE APPSETTING CLASS

--- bioku/src/app/_config/AppSetting.ts
+++ bioku/src/app/_config/AppSetting.ts
@@ -1,67 +1,68 @@
 export const AppSetting = {
 
-    //CHECK THESE BEFORE PRODUCTION BUILD
-    //API REST URL
+    // CHECK THESE BEFORE PRODUCTION BUILD
+    // API REST URL
     'URL': 'http://127.0.0.1:8000',
-    //app url
+    // APP URL FOR URL LINK TO THE FRONT-END
     'APP_URL': 'http://127.0.0.1:4200',
-    //FOR SENDINFG RESET PASSWORD
+    // FOR SENDINFG RESET PASSWORD
     'APP_DEFAULT_EMAIL': 'b.liu@bioku.nl',
 
-    //APP VERSION
+    // APP VERSION
     'VERSION': '0.0.1',
     ///////////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////DO NOT CHANGE BE IF NOT SURE ////////////////////////
-    'NAME':'BIOKU',
-    //APP CONSTANTS
+    ////////////////////////////////////////// DO NOT CHANGE BE IF NOT SURE ////////////////////////
+    'NAME': 'BIOKU',
+    // APP CONSTANTS
     'CONTAINER_FULLNESS_OVERVIEW_TOWER_PER_TABLE': 10,
     'BOX_VERTICAL': 8,
     'BOX_HORIZONTAL': 8,
     'BOX_EXTRA_LAYOYT': 8,
-    //image size
+    // image size
     'CONTAINER_IMAGE_WIDTH': 640,
     'CONTAINER_IMAGE_HEIGHT': 640,
-    //TOKEN EXPIRATION
+    // TOKEN EXPIRATION
     'TOKEN_EXPIRATION_HOUR': 6,
-    //show user defined box label?
+    // show user defined box label?
     'SHOW_BOX_LABEL': true,
-    //'APP_COLORS': ['#EF9A9A','#F48FB1','#CE93D8','#B39DDB','#9FA8DA','#90CAF9','#81D4FA','#80DEEA','#80CBC4','#A5D6A7','#C5E1A5','#E6EE9C','#FFF59D','#FFE082','#FFCC80','#FFAB91','#BCAAA4','#EEEEEE','#000000','#FFFFFF'],
-    'APP_COLORS': ['#F44336', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3','#03A9F4', '#00BCD4','#009688','#4CAF50','#8BC34A','#CDDC39','#FFEB3B','#FFC107','#FF9800','#FF5722','#795548','#9E9E9E','#607D8B','#000000', '#FFFFFF'],
-    'BOX_POSITION_LETTERS':['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-    'SAMPLE_TYPE':['GENERAL', 'CELL', 'CONSTRUCT', 'OLIGO', 'gRNA_OLIGO', 'TISSUE', 'VIRUS'], //DO NOT CHANGE THIS *****
-    //sample search keywords
+    'APP_COLORS': ['#F44336', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39',
+    '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#795548', '#9E9E9E', '#607D8B', '#000000', '#FFFFFF'],
+    'BOX_POSITION_LETTERS': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    'SAMPLE_TYPE': ['GENERAL', 'CELL', 'CONSTRUCT', 'OLIGO', 'gRNA_OLIGO', 'TISSUE', 'VIRUS'], // DO NOT CHANGE THIS *****
+    // sample search keywords
 
-    //////////////////////////////////API URLS////////////////////////////
-    //server reset password 
+    ////////////////////////////////// API URLS////////////////////////////
+    // server reset password
     'RESET_PASSWORD': '/reset_password/',
-    //confrim reset password
+    // confrim reset password
     'RESET_PASSWORD_CONFRIM': '/reset_password_confirm/',
-    //post to get token
+    // post to get token
     'TOKEN_URL': '/api/users/token/',
-    //get auth_user
+    // get auth_user
     'AUTH_USER': '/api/users/auth_user/',
-    //find username, user email and other info
-    'FIND_USER_DETAILS':'/api/users/search/',
-    //find group info
-    'FIND_GROUP_DETAILS':'/api/groups/search/',
-    //user register
+    // find username, user email and other info
+    'FIND_USER_DETAILS': '/api/users/search/',
+    // find group info
+    'FIND_GROUP_DETAILS': '/api/groups/search/',
+    // user register
     'REGISTER_USER': '/api/users/register/',
-    //find my groups with researchers
-    'AUTH_GROUPS':'/api/groups/mygroups/',
-    //user change password
+    // find my groups with researchers
+    'AUTH_GROUPS': '/api/groups/mygroups/',
+    // user change password
     'CHANGE_PASSWORD': '/api/users/password/',
-    //UPDATE_USER_PROFILE
+    // UPDATE_USER_PROFILE
     'UPDATE_USER_PROFILE': '/api/users/',
-    //GROUP API
+    // GROUP API
     'SINGLE_GROUP_API': '/api/groups/',
-    //UPDATE GROUP INFO
+    // UPDATE GROUP INFO
     'UPDATE_GROUP_INFO': '/api/groups/update/',
-    //find all groups
+    // find all groups
     'ALL_GROUPS': '/api/groups/',
-    //find all users
-    'ALL_USERS':'/api/users/',
-    //find all conatiners
+    // find all users
+    'ALL_USERS': '/api/users/',
+    // find all conatiners
     'ALL_CONTAINERS': '/api/containers/',
-    //FIND CONTAINER 
+    // FIND CONTAINER
     'FIND_CONTAINER_DETAILS': '/api/containers/search/',
 }
