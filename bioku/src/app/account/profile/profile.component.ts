971c24ab663688d242163996ffa7d03cb58ec0f1
b_liu <b.liu@nki.nl>
2017-10-15 22:21:21 +0200

    CHANGE THE COLOR THEMES FOR NON-ADMIN PARTS

--- bioku/src/app/account/profile/profile.component.ts
+++ bioku/src/app/account/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, AfterViewInit } from '@angular/core';
 import {FormBuilder, AbstractControl, FormGroup, Validators, FormControl} from '@angular/forms';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
@@ -10,17 +10,17 @@ import { UpdateUserProfileService } from '../../_services/UpdateUserProfileServi
 import { LogAppStateService } from '../../_services/LogAppStateService';
 import { RefreshService } from '../../_services/RefreshService';
 import { User } from '../../_classes/User';
-//mydatepicker
+// mydatepicker
 import {IMyOptions} from 'mydatepicker';
-//redux
+// redux
 import { AppStore } from '../../_providers/ReduxProviders';
 import { AppState , AppPartialState} from '../../_redux/root/state';
 import { REDUX_CONSTANTS as C } from '../../_redux/root/constants';
 import { updateProfileActionAsync } from '../../_redux/account/account_actions';
-//access dom
+// access dom
 import {ElementRef, ViewChild} from '@angular/core';
 
-//custom from validator
+// custom from validator
 import { CustomFormValidators } from '../../_helpers/CustomFormValidators';
 
 @Component({
@@ -28,23 +28,20 @@ import { CustomFormValidators } from '../../_helpers/CustomFormValidators';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
-
-  //access dom
-  @ViewChild('photoName') photoInput:ElementRef;
-
+export class ProfileComponent implements OnInit, AfterViewInit {
+  // access dom
+  @ViewChild('photoName') photoInput: ElementRef;
   profileForm: FormGroup;
   appName: string;
-  //for photo upload
+  // for photo upload
   file: File;
   photo_name: string;
   photo_is2Large: Boolean = false;
   photo_isSupported: Boolean = true;
-  
-  //auth user
+  // auth user
   user: User = null;
   token: string = null;
-  //mydatepicker
+  // mydatepicker
   myDatePickerOptions: IMyOptions = {
         // other options...
         todayBtnTxt: 'Today',
@@ -55,92 +52,90 @@ export class ProfileComponent implements OnInit {
         openSelectorOnInputClick: true};
 
   constructor(fb: FormBuilder, private alertService: AlertService, private refreshService: RefreshService,
-              @Inject(APP_CONFIG) private appSetting: any, @Inject(AppStore) private appStore, private updateUserProfileService: UpdateUserProfileService,
-              private router: Router, private logAppStateService: LogAppStateService, private cValidators: CustomFormValidators, private http: Http) {
-  //get the photo name for form
-  let state= this.appStore.getState();
+              @Inject(APP_CONFIG) private appSetting: any, @Inject(AppStore) private appStore,
+              private updateUserProfileService: UpdateUserProfileService,
+              private router: Router, private logAppStateService: LogAppStateService,
+              private cValidators: CustomFormValidators, private http: Http) {
+  // get the photo name for form
+  const state = this.appStore.getState();
   this.user = state.authInfo.authUser;
   this.token = state.authInfo.token.token;
-  let photo_path = this.user.profile.photo;
-  this.photo_name = this.user.profile.photo ? photo_path.split('/').pop(): '';
-  //parse date for form intial birth date
+  const photo_path = this.user.profile.photo;
+  this.photo_name = this.user.profile.photo ? photo_path.split('/').pop() : '';
+  // parse date for form intial birth date
   let init_date = {};
-  if(this.user.profile.birth_date){
-    let dArray = this.user.profile.birth_date.split('-');
+  if (this.user.profile.birth_date) {
+    const dArray = this.user.profile.birth_date.split('-');
     init_date = {date: {year: +dArray[0], month: +dArray[1], day: +dArray[2]}};
   }
-  //formGroup
+  // formGroup
     this.profileForm = fb.group({
-      'email': [this.user.email, Validators.compose([Validators.required, Validators.email]), this.cValidators.emailAsyncValidator(+this.user.pk)],
+      'email': [this.user.email,
+        Validators.compose([Validators.required, Validators.email]), this.cValidators.emailAsyncValidator(+this.user.pk)],
       'first_name': [this.user.first_name, Validators.compose([Validators.required, this.cValidators.humanNameValidator()])],
       'last_name': [this.user.last_name, Validators.compose([Validators.required, this.cValidators.humanNameValidator()])],
       'birth_date': [init_date, ],
       'photo': ['', ],
       'telephone': [this.user.profile.telephone, this.cValidators.telephoneValidator()]
     });
-    //app name
+    // app name
     this.appName = appSetting.NAME;
-    //subscribe store state changes
-    appStore.subscribe(()=> this.updateState());
+    // subscribe store state changes
+    appStore.subscribe(() => this.updateState());
     this.updateState();
-    
   }
-
   ngAfterViewInit() {
-    //update photo name to the template
+    // update photo name to the template
     this.photoInput.nativeElement.value = this.photo_name;
   }
-  
-  //check upload photo
+  // check upload photo
   validatePhotoUpload(event: EventTarget) {
-        let eventObj: MSInputMethodContext = <MSInputMethodContext> event;
-        let target: HTMLInputElement = <HTMLInputElement> eventObj.target;
-        let files: FileList = target.files;
-        this.file = files[0];
-        //console.log(this.file);
-        this.photo_name = this.file.name;
-        //check file size
-        let size = this.file.size / 1024 / 1024
-        if (parseInt(size.toFixed(2)) > 10) {
-            this.photo_is2Large = true;
-        }
-        else{
-          this.photo_is2Large = false;
-        }
-        //check image format
-        if (this.file.type !== "image/png" && this.file.type  !== "image/jpeg" && this.file.type  !== "image/bmp" && this.file.type  !== "image/gif") {
-            this.photo_isSupported =  false;
-        }
-        else{
-          this.photo_isSupported =  true;
-        }
+    const eventObj: MSInputMethodContext = <MSInputMethodContext> event;
+    const target: HTMLInputElement = <HTMLInputElement> eventObj.target;
+    const files: FileList = target.files;
+    this.file = files[0];
+    // console.log(this.file);
+    this.photo_name = this.file.name;
+    // check file size
+    const size = this.file.size / 1024 / 1024
+    if (parseInt(size.toFixed(2), 10) > 10) {
+        this.photo_is2Large = true;
+    } else {
+      this.photo_is2Large = false;
+    }
+    // check image format
+    if (this.file.type !== 'image/png' && this.file.type  !== 'image/jpeg' &&
+        this.file.type  !== 'image/bmp' && this.file.type  !== 'image/gif') {
+      this.photo_isSupported =  false;
+    }else {
+      this.photo_isSupported =  true;
+    }
   }
-  
-    onUpdate(values: any): void{
-      console.log(this.profileForm);
-    let obj = {
+
+  onUpdate(values: any): void {
+    const obj = {
             email: values.email,
             first_name : values.first_name,
             last_name : values.last_name,
-            birth_date : values.birth_date.date.year + '-'+ values.birth_date.date.month + '-'+ values.birth_date.date.day,
+            birth_date : values.birth_date.date.year + '-' + values.birth_date.date.month + '-' + values.birth_date.date.day,
             telephone : values.telephone };
     console.log(obj);
-    let formData: FormData = new FormData();
-    formData.append("obj", JSON.stringify(obj));
-    if (this.file){
-      formData.append("file", this.file, this.file.name);
+    const formData: FormData = new FormData();
+    formData.append('obj', JSON.stringify(obj));
+    if (this.file) {
+      formData.append('file', this.file, this.file.name);
     }
-    //put call
+    // put call
     this.appStore.dispatch(updateProfileActionAsync(formData, +this.user.pk, this.token, this.updateUserProfileService, this.http, this.logAppStateService, this.alertService, this.refreshService));
-    //naviagate to home
-    this.router.navigate(['/']);    
+    // naviagate to home
+    this.router.navigate(['/']);
   }
 
-  updateState(){
-    let state= this.appStore.getState();
+  updateState() {
+    const state = this.appStore.getState();
     this.user = state.authInfo.authUser;
     this.token = state.authInfo.token.token;
-    //console.log(state);
+    // console.log(state);
   }
 
   ngOnInit() {}
