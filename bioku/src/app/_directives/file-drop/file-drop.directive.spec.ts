79999dec85f1d041a339d7cd18b1ebd0a6797b3d
b_liu <b.liu@nki.nl>
2017-10-29 22:05:42 +0100

    remove file-drop test

--- bioku/src/app/_directives/file-drop/file-drop.directive.spec.ts
+++ bioku/src/app/_directives/file-drop/file-drop.directive.spec.ts
@@ -1,10 +1,9 @@
 import { FileDropDirective } from './file-drop.directive';
 import {  AlertService } from '../../_services/AlertService';
-import { Router } from '@angular/router';
 
-describe('FileDropDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FileDropDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+// describe('FileDropDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new FileDropDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
