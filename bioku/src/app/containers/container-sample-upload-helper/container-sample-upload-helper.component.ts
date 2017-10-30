b98c39fcc33be9a4d6fed5e599c7911ac1f835a8
b_liu <b.liu@nki.nl>
2017-10-30 16:33:35 +0100

    wokring on step 1 form

--- bioku/src/app/containers/container-sample-upload-helper/container-sample-upload-helper.component.ts
+++ bioku/src/app/containers/container-sample-upload-helper/container-sample-upload-helper.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container-sample-upload-helper.component.css']
 })
 export class ContainerSampleUploadHelperComponent implements OnInit {
-
+  box_defined_as_normal: Boolean = true;
+  box_tsb_one_column: Boolean = true; // "tower", "shelf" and "box" in one data column
+  box_sample_separated: Boolean = true; // box position is separated from sample position in my excel
+  activeStep: Number = 1;
   constructor() { }
 
   ngOnInit() {
   }
-
+  toggleBoxPositionDefinition() {
+    this.box_defined_as_normal = !this.box_defined_as_normal;
+  }
+  toggleBoxTSBColumn() {
+    this.box_tsb_one_column = !this.box_tsb_one_column;
+  }
+  toggleBoxSampleSeparated() {
+    this.box_sample_separated = !this.box_sample_separated;
+  }
 }
