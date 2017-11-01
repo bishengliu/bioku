2b828a7a706c02689048d10ef9f3faf8357887aa
b_liu <b.liu@nki.nl>
2017-11-01 23:24:01 +0100

    need to use dragular to sepecify columns

--- bioku/src/app/containers/container-sample-uploader-step-three/container-sample-uploader-step-three.component.ts
+++ bioku/src/app/containers/container-sample-uploader-step-three/container-sample-uploader-step-three.component.ts
@@ -28,6 +28,7 @@ export class ContainerSampleUploaderStepThreeComponent implements OnInit {
   parsing_file: Boolean = false;
 
   sampleFile: SampleFile = new SampleFile();
+  column_headers: Array<string> = [];
   sample_type = 'GENERAL';
   max_column_count = 10;
   column_count = 10;
@@ -71,7 +72,8 @@ export class ContainerSampleUploaderStepThreeComponent implements OnInit {
   }
   updateSampleType(sample_type: string): void {
     this.sample_type = sample_type === '' ? this.sample_type : sample_type;
-    ////////////need to get column count and data attrs ////////////
+    //////////// need to get column count and data attrs ////////////
+    this.column_headers = this.updateSampleAttr(this.sample_type, this.bLabel, this.sLabel);
 
   }
   toggleExcelFilerHeader() {
@@ -82,10 +84,73 @@ export class ContainerSampleUploaderStepThreeComponent implements OnInit {
   }
   setDefaultSampleFile() {
     this.sample_type = 'GENERAL';
-    this.max_column_count = 10;
-    this.column_count = 10;
     this.excel_file_has_header = true;
-
     // set up column count and headers
+    this.column_headers = this.updateSampleAttr(this.sample_type, this.bLabel, this.sLabel);
+    this.max_column_count = this.column_headers.length;
+    this.column_count = this.column_headers.length;
   }
+  updateSampleAttr (sample_type: string, bLabel: BoxLabel, sLabel: SampleLabel): Array<string> {
+    let sample_headers: Array<string> = [];
+    // box label
+    if (bLabel.box_defined_as_normal && bLabel.box_tsb_one_column) {
+      sample_headers.push('BoxLabel');
+      // 1 col for box label
+    } else if (bLabel.box_defined_as_normal && !bLabel.box_tsb_one_column) {
+      sample_headers.push('BoxLabel_Tower');
+      sample_headers.push('BoxLabel_Shelf');
+      sample_headers.push('BoxLabel_Box');
+      // 3 col for box label
+    } else if (!bLabel.box_defined_as_normal && bLabel.box_sample_separated) {
+      // 1 col for box label
+      sample_headers.push('BoxLabel');
+    } else {
+      // no box lable col
+    }
+    // sample label
+    if (sLabel.sampleLabelDefinition === 2) {
+      // 2 cols for sample label
+      sample_headers.push('SampleLabel_Column');
+      sample_headers.push('SampleLabel_Row');
+    } else {
+      // 1 col for sample label
+      sample_headers.push('SampleLabel');
+    }
+    const general_headers: Array<string> = ['Name', 'Tag', 'Offical Name', 'Sample Code', 'External Reference', 'quantity',
+                                            'quantity Unit', 'Freezing Code', 'Freezing Date', 'Description'];
+    const cell_headers: Array<string> = ['Passage Number', 'Cell Cmount', 'Project', 'Creator'];
+    const construct_headers: Array<string> = ['Clone Number', '260/280', 'Feature', 'R.E. Analysis', 'Backbone', 'Insert',
+                                              'ist maxi', 'Marker', 'Has Glycerol Stock', 'Stock Strain'];
+    const oligo_headers: Array<string> = ['Oligo Name', 'Sense/Antisense', 'Oligo Sequence', 'Oligo Length', 'GC%', 'Target Sequence'];
+    const tissue_headers: Array<string> = ['Pathology Code', 'Tissue'];
+    const virus_headers: Array<string> = ['Plasmid', 'Titration Titer', 'Titration Unit', 'Titration Cell Type', 'Titration Code'];
+    switch (sample_type) {
+      case 'GENERAL':
+        sample_headers = [...general_headers];
+        break;
+      case 'CELL':
+        sample_headers = [...general_headers, ...cell_headers];
+        break;
+      case 'CONSTRUCT':
+        sample_headers = [...general_headers, ...construct_headers];
+        break;
+      case 'OLIGO':
+        sample_headers = [...general_headers, ...oligo_headers];
+        break;
+      case 'gRNA_OLIGO':
+        sample_headers = [...general_headers, ...oligo_headers];
+        break;
+      case 'TISSUE':
+        sample_headers = [...general_headers, ...tissue_headers];
+        break;
+      case 'VIRUS':
+        sample_headers = [...general_headers, ...virus_headers];
+        break;
+      default:
+        sample_headers = [...general_headers];
+        break;
+    }
+    return sample_headers;
+  }
+  /////////////////need to map header to sample table attrs/////////////////////
 }
