a8d3b1161c4cbf46cf6eb6b9ab6b938f32e8034f
b_liu <b.liu@nki.nl>
2018-02-21 23:49:18 +0100

    switch to dev mode

--- bioku/src/app/_config/AppSetting.ts
+++ bioku/src/app/_config/AppSetting.ts
@@ -3,18 +3,18 @@ export const AppSetting = {
     ////////////////////////////////////////// CHECK THESE BEFORE PRODUCTION BUILD/////////////////
     // API BACKEND REST URL
     // amazon testing server backend server
-    'URL': 'http://34.216.52.204:8000',
+    // 'URL': 'http://34.216.52.204:8000',
     // local development
-    // 'URL': 'http://127.0.0.1:8000',
+    'URL': 'http://127.0.0.1:8000',
     // dell test
     // 'URL': 'http://192.168.0.105:8000',
     // 'URL': 'http://127.0.0.1/biodataware',
 
     // APP URL FOR URL LINK TO THE FRONT-END ////////////
     // amazon testing server content server
-    'APP_URL': 'http://34.216.52.204',
+    // 'APP_URL': 'http://34.216.52.204',
     // local development
-    // 'APP_URL': 'http://127.0.0.1:4200',
+    'APP_URL': 'http://127.0.0.1:4200',
     // dell test
     // 'APP_URL': 'http://192.168.0.105',
 
